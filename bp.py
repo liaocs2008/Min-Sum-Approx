cce84280dfd792fa9cd11eb32d213ade568acd57
leo <liaocs2008@gmail.com>
2020-09-03 22:56:01 -0400

    add lch support

--- bp.py
+++ bp.py
@@ -5,6 +5,7 @@ from utils import *
 
 parser = argparse.ArgumentParser("Belief Propagation Decoder")
 parser.add_argument('--B', type=int, default=64, help='Batch size')
+parser.add_argument('--C', type=str, default='bp', help='bp or lch')
 parser.add_argument('--F', type=str, default='offset', help='Functions to select')
 parser.add_argument('--G', type=str, default='./hmatrix/BCH_63_51_2_strip_G.npy', help='Generator matrix')
 parser.add_argument('--H', type=str, default='./hmatrix/BCH_63_51_2_strip_H.npy', help='Parity Check matrix')
@@ -17,6 +18,7 @@ parser.add_argument('--s_snr', default=1.0, type=float, help='SNR start value in
 parser.add_argument('--e_snr', default=6.0, type=float, help='SNR end value in dB')
 parser.add_argument('--step', default=1.0, type=float, help='SNR step in dB')
 parser.add_argument('--offset', default=0.3, type=float, help='Offset min sum value')
+parser.add_argument('--coef', default=0.9375, type=float, help='Scaled min sum value')
 parser.add_argument('--seed', type=int, default=2020, help='Random seed')
 
 
@@ -37,6 +39,8 @@ if __name__ == "__main__":
     # select functions
     if args.F == "offset":
         f = partial(offset_min_sum, offset=args.offset)
+    elif args.F == "scaled":
+        f = partial(scaled_min_sum, coef=args.coef)
     else:
         f = None
     
@@ -49,7 +53,11 @@ if __name__ == "__main__":
             me = np.random.randint(2, size=[args.B, args.K])
             no = np.random.randn(args.B, args.N)
             Lch, x = encode(G, me, SNR_in_db, no)
-            binary_d = decode(Lch, info, args.K, args.N, args.T, f)
+            #print("llr", Lch.sum())
+            if args.C == 'bp':
+                binary_d = decode(Lch, info, H.shape[1], args.N, args.T, f)
+            else:
+                binary_d = (Lch < 0).astype(np.int)
             res = binary_d != x  # (batch, N)
             
             num_batches += 1
@@ -59,5 +67,9 @@ if __name__ == "__main__":
         # final output
         BER = diff_ber / (num_batches * args.B * args.N)
         FER = diff_fer / (num_batches * args.B)
-        print(args.F, args.offset, "N={} K={} SNR_in_db={:.2f} BER={:.6f} FER={:.6f}".format(
-              args.N, args.K, SNR_in_db, BER, FER))
+        out = "N= {} K= {} SNR_in_db= {:.2f} BER= {:.6f} FER= {:.6f}".format(
+              args.N, args.K, SNR_in_db, BER, FER)
+        if args.F == 'offset':
+            print(args.F, args.offset, out)
+        else:
+            print(args.F, args.coef, out)
